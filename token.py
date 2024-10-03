3f8fd387f0c952b626166a20b360ecbec330143b
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-03 15:31:01 +0530

    commit

---
+++ token.py
@@ -0,0 +1,28 @@
+print(23%4)
+print(4%23)
+#floor division operator (//)
+print(23/4) # 5.75
+print(23//4) # 5
+print(60//7) # 8
+print(60//6) # 10
+print(7**3) #343
+
+#comparasion operator
+print(23>45) #F
+print(23>=45) #f
+print(23<=45) #T
+print(23==45) #F
+print(23!=45) #T
+
+# string comparasion
+
+print("apple" > "banana")
+#ascii(american standard code for information interchange) value
+# A = 65
+# a = 97
+# 0 = 48
+
+# "apple" > "applf"
+# it is called lexicographical comparision
+print("apple" > "applf")
+print("apple" == "apple")
