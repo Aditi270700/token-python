1e07d6095605acc229b2f7b10e978183685e5fbd
aditi saudagar <aditisaudagar17@gmail.com>
2024-10-05 22:40:13 +0530

    data type

---
+++ 5 oct/data_type.py
@@ -0,0 +1,21 @@
+#Data-Type
+
+'''
+Numerals    Sequence     Mapping     Set      None
+int          list        dictionary  set      None
+float        tuple
+complex      string
+bool
+'''
+#   Input operation
+'''
+number1 = int(input("enter 1st number:"))
+number2 = int(input("enter 2nd  number:"))
+
+print("Add",number1+number2)
+'''
+
+Name = input("Enter the name:")
+Surname = input("Enter the surname:")
+
+print("name",Name+" "+Surname)
---
+++ 5 oct/newfile.py
@@ -0,0 +1,12 @@
+
+number1 = 100
+number2 = 200
+number3 = 100
+
+print(id(number1),id(number2),id(number3)) #Garbage collector(GC)
+#print(number1/2)
+number1 = 98
+print(id(number1),(number2),(number3))
+number3 = 567
+
+number1 = "wwwwwwwwwwwwwwwwww"
